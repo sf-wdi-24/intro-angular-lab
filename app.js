8b654ef2fc7dc0854f59a2cf5f3983db1e1d55b7
tkhuynh <thequest2311@yahoo.com>
2016-01-19 19:56:39 -0800

    add form to create new movie, display 5 movies, add delete movie

--- app.js
+++ app.js
@@ -20,6 +20,24 @@ app.controller('MovieCtrl', ['$scope', function($scope) {
 			title: "Agent 47",
 			year: 2015,
 			poster: "http://40.media.tumblr.com/0eee1678d27e6575e7b4c9eac0d49e40/tumblr_nszhmgdL6m1ri74e4o3_500.jpg"
+		},
+		{
+			title: "The Martian",
+			year: 2015,
+			poster: "http://thefilmstage.com/wp-content/uploads/2015/09/pp102015_martian01.jpg"
+		},
+		{
+			title: "Creed",
+			year: 2015,
+			poster: "http://static1.squarespace.com/static/548fddcde4b0bbd467f73559/t/568c32ba5a56689becb297b4/1452028604332/Creed+Movie+Poster?format=500w"
 		}
 	];
+	$scope.addMovie = function () {
+		$scope.moviesToWatch.push($scope.newMovie);
+		$scope.newMovie = {};
+	};
+	$scope.deleteMovie = function(movie) {
+		movieToBeDeleteIndex = $scope.moviesToWatch.indexOf(movie);
+		$scope.moviesToWatch.splice(movieToBeDeleteIndex, 1);
+	};
 }]);
