21b80f1b546db907f24f11a80e2f6c9d56f51921
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-01-19 16:59:28 -0800

    nice table going, next is adding a movie

---
+++ app.js
@@ -0,0 +1,38 @@
+var app = angular.module("myApp", []);
+
+app.controller("MovieCtrl", function($scope) {
+	$scope.moviesToWatch = [{
+		title: 'Titantic',
+		rating: 'R',
+		type: 'Romance'
+	}, {
+		title: 'Ex-Machina',
+		rating: 'R',
+		type: 'SciFi'
+	}, {
+		title: 'Star Wars',
+		rating: 'PG-13',
+		type: 'SciFi'
+	}, {
+		title: 'Star Trek',
+		rating: 'R',
+		type: 'SciFi'
+	}, {
+		title: 'Inside Out',
+		rating: 'G',
+		type: 'Animated'
+	}];
+
+	$scope.movie = {};
+	$scope.addMovie = function() {
+		var newMovie = $scope.moviesToWatch;
+		$scope.movie = {};
+		$scope.moviesToWatch.push(newMovie);
+	};
+
+	$scope.deleteMovie = function(movie) {
+		var movieIndex = $scope.movies.indexOf(movie);
+		$scope.movies.splice(moviesIndex, 1);
+	};
+
+});
